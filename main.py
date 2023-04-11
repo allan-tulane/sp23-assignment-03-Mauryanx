251a3d861e22ca2d4fd47ea24732ffdc76da02ca
Mauryanx <115839207+Mauryanx@users.noreply.github.com>
2023-04-11 09:53:28 -0500

    Finished

--- main.py
+++ main.py
@@ -42,9 +42,11 @@ def parens_match_iterative(mylist):
     >>>parens_match_iterative(['('])
     False
     """
-    ### TODO
-    pass
-
+  x = iterate(parens_update, 0, mylist)
+  if (x == 0):
+    return True
+  else:
+    return False
 
 def parens_update(current_output, next_input):
     """
@@ -58,8 +60,13 @@ def parens_update(current_output, next_input):
     Returns:
       the updated value of `current_output`
     """
-    ###TODO
-    pass
+  if (next_input == "("):
+    current_output += 1
+  elif (current_output < 1 and next_input == ")"):
+    current_output = -99
+  elif (next_input == ")"):
+    current_output -= 1
+  return current_output
 
 
 def test_parens_match_iterative():
@@ -87,8 +94,19 @@ def parens_match_scan(mylist):
     False
     
     """
-    ###TODO
-    pass
+    m = list(map(lambda x:paren_map(x), mylist))
+  for i in m:
+    if (i == -1):
+      return False
+    if (i == 1):
+      break
+
+  s = scan(plus, 0, m)[0]
+  r = reduce(min_f, 0, s)
+  if (r != 0):
+    return False
+  else:
+    return True
 
 def scan(f, id_, a):
     """
@@ -136,6 +154,9 @@ def test_parens_match_scan():
     assert parens_match_scan(['(', ')']) == True
     assert parens_match_scan(['(']) == False
     assert parens_match_scan([')']) == False
+    
+ def plus(x, y):
+  return x + y
 
 #### Divide and conquer solution
 
@@ -160,8 +181,23 @@ def parens_match_dc_helper(mylist):
       L is the number of unmatched left parentheses. This output is used by 
       parens_match_dc to return the final True or False value
     """
-    ###TODO
-    pass
+  if len(mylist) == 0:
+    return (0, 0)
+  elif len(mylist) == 1:
+    if (mylist[0] == "("):
+      return (0, 1)
+    elif (mylist[0] == ")"):
+      return (1, 0)
+    else:
+      return (0, 0)
+  else:
+    rr, rl = parens_match_dc_helper(mylist[(len(mylist)//2):])
+    ll, lr = parens_match_dc_helper(mylist[(len(mylist)//2):])
+    if ll == 0 and rr == 0 and lr > 0 and rl > 0:
+      return (lr, rl)
+    x = rr - ll
+    y = rl - lr
+    return (x, y)
     
 
 def test_parens_match_dc():
